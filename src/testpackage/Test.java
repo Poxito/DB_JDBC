e0e50240dca0c0e977809196ad68fbf0dee39915
estrapro <mortbg@gmail.com>
2021-05-10 12:37:32 +0200

    Let's start this

---
+++ src/testpackage/Test.java
@@ -0,0 +1,37 @@
+package testpackage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // https://docs.oracle.com/javase/8/docs/api/java/sql/package-summary.html#package.description
+        // auto java.sql.Driver discovery -- no longer need to load a java.sql.Driver class via Class.forName
+
+        // register JDBC driver, optional since java 1.6
+        /*try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }*/
+
+        // auto close connection
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://dif-mysql.ehu.es:3306/dbdi24", "DBDI24", "laiotz2001")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/testpackage/Test2.java
@@ -0,0 +1,49 @@
+package testpackage;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.Employee;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("MySQL JDBC Connection Testing ~");
+
+        List<Employee> result = new ArrayList<>();
+
+        String SQL_SELECT = "Select * from employee";
+
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://dif-mysql.ehu.es:3306/dbdi24", "DBDI24", "laiotz2001");
+             PreparedStatement preparedStatement = conn.prepareStatement(SQL_SELECT)) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                String id = resultSet.getString("Ssn");
+                String name = resultSet.getString("Fname");
+                BigDecimal salary = resultSet.getBigDecimal("Salary");
+
+                Employee obj = new Employee();
+                obj.setId(id);
+                obj.setName(name);
+                obj.setSalary(salary);
+
+                result.add(obj);
+
+            }
+            result.forEach(x -> System.out.println(x.getId() + " " + x.getName() + " " + x.getSalary()));
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
