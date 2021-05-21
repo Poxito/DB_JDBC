e3f1764d93a66a8fdcc2435facb4eaaf6e9f3901
estrapro <mortbg@gmail.com>
2021-05-21 12:35:51 +0200

    Queries class created and GUI fully functional

---
+++ src/domain/Queries.java
@@ -0,0 +1,66 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Queries {
+
+	private ArrayList<String> AritzQueryText = new ArrayList<String>();
+	private ArrayList<String> AritzQueries = new ArrayList<String>();
+	private ArrayList<String> ZdravkoQueryText = new ArrayList<String>();
+	private ArrayList<String> ZdravkoQueries = new ArrayList<String>();
+
+	public Queries() {
+
+		AritzQueryText.add(
+				"Retrieve the people that eat the dish 'cheescake' or a dish which its difficulty is higher or equal than 1, grouped by the category of the dish.");
+		AritzQueryText.add(
+				"Retrieve the maximum, minimum and average of hours of those employees that work in any project of department 1.");
+		AritzQueryText.add("People who frequents the same restaurant(s) as Kevin.");
+		AritzQueries.add("select p.nameId, d.category "
+				+ "from eats as e inner join person as p on e.nameId = p.nameId inner join dishes as d on e.dish = d.dish "
+				+ "where d.dish = \"cheesecake\" or d.dish in (select di.dish " + "from dishes as di "
+				+ "where di.category >= 1.0) " + "group by p.nameId, d.category");
+		AritzQueries.add("select em.Fname, min(wo.Hours), max(wo.Hours), avg(wo.Hours) "
+				+ "from works_on as wo inner join employee as em on wo.Essn = em.Ssn "
+				+ "where wo.Pno = (select pr.Pnumber " + "from project as pr " + "where pr.Dnum = 1) "
+				+ "group by em.Fname, wo.Hours, em.Lname " + "order by em.Lname");
+		AritzQueries.add("select distinct fre.nameId " + "from frequents as fre " + "where not exists (select *  "
+				+ "from (select restaurname " + "from frequents " + "where nameId = \"Kevin\") as KR "
+				+ "where KR.restaurname not in (select restaurname " + "from frequents as freque "
+				+ "where freque.nameId = fre.nameId))");
+		ZdravkoQueryText.add("Retrieve the name and the trips of the guides who speak Basque and have gone to Beirut.");
+		ZdravkoQueryText
+				.add("Retrieve the all the information of the customer(s) that have gone to Mars and not to Everest.");
+		ZdravkoQueryText
+				.add("Retrieve the first name and last name of the those employees that work in project 1 and 2.");
+		ZdravkoQueries.add("select distinct tou.guidename, tr.TripTo "
+				+ "from (tourguide as tou inner join languages as lang on tou.GuideId = lang.GuideId) "
+				+ " inner join trip as tr on tou.GuideId = tr.GuideId "
+				+ "where lang.Lang =\"Euskera\" and tou.GuideId in (select trip.GuideId " + "from trip "
+				+ "where trip.TripTo = \"Beirut\")");
+		ZdravkoQueries.add("select c.CustomerId, c.custname, c.custaddress, c.custphone " + "from customer as c "
+				+ "where exists(select * from hotel_trip_customer as htc where htc.CustomerId = c.CustomerId and htc.TripTo = \"Mars\") "
+				+ "and not exists(select * from hotel_trip_customer as htc_vive where htc_vive.CustomerId = c.CustomerId and htc_vive.TripTo = \"Everest\")");
+		ZdravkoQueries.add("select em.Fname, em.Lname " + "from employee as em "
+				+ "where exists(select * from works_on as wo1 where wo1.Essn = em.Ssn and wo1.Pno = 1) "
+				+ "and exists(select * from works_on as wo2 where wo2.Essn = em.Ssn and wo2.Pno = 2)");
+	}
+
+	public ArrayList<String> getAritzQueryText() {
+		return AritzQueryText;
+	}
+
+	public ArrayList<String> getAritzQueries() {
+		return AritzQueries;
+	}
+
+	public ArrayList<String> getZdravkoQueryText() {
+		return ZdravkoQueryText;
+	}
+
+	public ArrayList<String> getZdravkoQueries() {
+		return ZdravkoQueries;
+	}
+	
+	
+}
--- src/presentation/MainGUI.java
+++ src/presentation/MainGUI.java
@@ -2,24 +2,37 @@ package presentation;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import domain.Queries;
+
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollBar;
+import javax.swing.JTable;
+import javax.swing.JList;
+import javax.swing.JTextArea;
 
 public class MainGUI extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtQueryTextWill;
 	private ButtonGroup buttonGroup = new ButtonGroup();
+	JTextArea textArea = new JTextArea();
+	private JTable table;
+	private Queries queries = new Queries();
+	private int actualIndex = 0;
 
 	/**
 	 * Launch the application.
@@ -41,8 +54,9 @@ public class MainGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MainGUI() {
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 524, 300);
+		setBounds(100, 100, 524, 383);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -53,29 +67,80 @@ public class MainGUI extends JFrame {
 		contentPane.add(lblWichStudentsWork);
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(comboBox.getSelectedItem().equals("Aritz Plazaola")) textArea.setText(queries.getAritzQueryText().get(actualIndex));
+				if(comboBox.getSelectedItem().equals("Zdravko Todorov")) textArea.setText(queries.getZdravkoQueryText().get(actualIndex));
+			}
+		});
 		comboBox.setBounds(323, 13, 189, 24);
 		contentPane.add(comboBox);
 		
+		comboBox.addItem("Please select student");
 		comboBox.addItem("Aritz Plazaola");
 		comboBox.addItem("Zdravko Todorov");
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
 		
-		txtQueryTextWill = new JTextField();
-		txtQueryTextWill.setText("Query text will appear here");
-		txtQueryTextWill.setBounds(30, 123, 456, 90);
-		contentPane.add(txtQueryTextWill);
-		txtQueryTextWill.setColumns(10);
-		txtQueryTextWill.setVisible(false);
+		textArea.setText("Query text will appear here");
+		textArea.setBounds(30, 103, 456, 204);
+		contentPane.add(textArea);
+		textArea.setVisible(false);
+		textArea.setFont(new Font("Dialog", Font.BOLD, 14));
+		
+		DefaultListModel<String> model = new DefaultListModel<String>();
+		model.addElement("Online People");
+		model.addElement("talking to earth");
+		JList<String> list = new JList<String>(model);
+		
+		list.setBounds(30, 103, 456, 204);
+		contentPane.add(list);
+		list.setVisible(false);
 		
 		JButton btnNextQuery = new JButton("Next Query");
-		btnNextQuery.setBounds(350, 225, 136, 25);
+		btnNextQuery.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(actualIndex < 2) actualIndex++;
+				if(comboBox.getSelectedItem().equals("Aritz Plazaola") && actualIndex < 3) textArea.setText(queries.getAritzQueryText().get(actualIndex));
+				if(comboBox.getSelectedItem().equals("Zdravko Todorov") && actualIndex < 3) textArea.setText(queries.getZdravkoQueryText().get(actualIndex));
+			}
+		});
+		btnNextQuery.setBounds(350, 319, 136, 25);
 		contentPane.add(btnNextQuery);
 		
 		JButton btnPreviousQuery = new JButton("Previous Query");
-		btnPreviousQuery.setBounds(30, 225, 141, 25);
+		btnPreviousQuery.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(actualIndex > 0) actualIndex--;
+				if(comboBox.getSelectedItem().equals("Aritz Plazaola") && actualIndex >= 0) textArea.setText(queries.getAritzQueryText().get(actualIndex));
+				if(comboBox.getSelectedItem().equals("Zdravko Todorov") && actualIndex >= 0) textArea.setText(queries.getZdravkoQueryText().get(actualIndex));
+			}
+		});
+		btnPreviousQuery.setBounds(30, 319, 141, 25);
 		contentPane.add(btnPreviousQuery);
 		
 		JButton btnExecute = new JButton("Execute");
-		btnExecute.setBounds(203, 225, 117, 25);
+		btnExecute.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(!btnExecute.getText().equals("Back")) {
+				textArea.setVisible(false);
+				list.setVisible(true);
+				btnExecute.setText("Back");
+				btnNextQuery.setVisible(false);
+				btnPreviousQuery.setVisible(false);
+				} else {
+					
+					textArea.setVisible(true);
+					list.setVisible(false);
+					btnExecute.setText("Execute");
+					btnNextQuery.setVisible(true);
+					btnPreviousQuery.setVisible(true);
+				}
+			}
+		});
+		btnExecute.setBounds(203, 319, 117, 25);
 		contentPane.add(btnExecute);
 		
 		btnExecute.setVisible(false);
@@ -90,7 +155,7 @@ public class MainGUI extends JFrame {
 		rdbtnQuery.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				txtQueryTextWill.setVisible(true);
+				textArea.setVisible(true);
 				btnExecute.setVisible(true);
 				btnNextQuery.setVisible(true);
 				btnPreviousQuery.setVisible(true);
@@ -103,7 +168,7 @@ public class MainGUI extends JFrame {
 		rdbtnTransaction.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			
-				txtQueryTextWill.setVisible(false);
+				textArea.setVisible(false);
 				btnExecute.setVisible(false);
 				btnNextQuery.setVisible(false);
 				btnPreviousQuery.setVisible(false);
